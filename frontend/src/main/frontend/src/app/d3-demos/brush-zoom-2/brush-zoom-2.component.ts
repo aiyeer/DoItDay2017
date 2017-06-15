cfc25edbfa519144974f7ed3a21508632765e829
Akhil Gudimella <agudimel@tibco.com>
2017-06-15 16:59:39 -0700

    Line chart added

--- frontend/src/main/frontend/src/app/d3-demos/brush-zoom-2/brush-zoom-2.component.ts
+++ frontend/src/main/frontend/src/app/d3-demos/brush-zoom-2/brush-zoom-2.component.ts
@@ -6,157 +6,87 @@
 import { Component, ElementRef, NgZone, OnDestroy, OnInit } from '@angular/core';
 
 
-import {
-  D3Service,
-  D3,
-  Axis,
-  BrushBehavior,
-  BrushSelection,
-  D3BrushEvent,
-  ScaleLinear,
-  ScaleOrdinal,
-  Selection,
-  Transition
-} from 'd3-ng2-service';
+import * as d3 from 'd3-selection';
+import * as d3Scale from "d3-scale";
+import * as d3Shape from "d3-shape";
+import * as d3Array from "d3-array";
+import * as d3Axis from "d3-axis";
+
+import { Stocks } from './shared/data';
 
 
 @Component({
   selector: 'app-brush-zoom-2',
-  template: '<svg width="500" height="400"></svg>'
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{subtitle}}</h2>
+    <svg width="500" height="500"></svg>
+  `
 })
-export class BrushZoom2Component implements OnInit, OnDestroy {
-  private d3: D3;
-  private parentNativeElement: any;
-  private d3Svg: Selection<SVGSVGElement, any, null, undefined>;
+export class BrushZoom2Component implements OnInit {
 
-  constructor(element: ElementRef, private ngZone: NgZone, d3Service: D3Service) {
-    this.d3 = d3Service.getD3();
-    this.parentNativeElement = element.nativeElement;
-  }
+  private margin = {top: 20, right: 20, bottom: 30, left: 50};
+  private width: number;
+  private height: number;
+  private x: any;
+  private y: any;
+  private svg: any;
+  private line: d3Shape.Line<[number, number]>;
 
-  ngOnDestroy() {
-    if (this.d3Svg.empty && !this.d3Svg.empty()) {
-      this.d3Svg.selectAll('*').remove();
-    }
+  constructor() {
+    this.width = 500 - this.margin.left - this.margin.right ;
+    this.height = 500 - this.margin.top - this.margin.bottom;
   }
 
   ngOnInit() {
-    let self = this;
-    let d3 = this.d3;
-    let d3ParentElement: Selection<HTMLElement, any, null, undefined>;
-    let d3Svg: Selection<SVGSVGElement, any, null, undefined>;
-    let d3G: Selection<SVGGElement, any, null, undefined>;
-    let width: number;
-    let height: number;
-    let random: () => number;
-    let sqrt3: number;
-    let points0: Array<[number, number, number]>;
-    let points1: Array<[number, number, number]>;
-    let points2: Array<[number, number, number]>;
-    let points: Array<[number, number, number]>;
-    let k: number;
-    let x0: [number, number];
-    let y0: [number, number];
-    let x: ScaleLinear<number, number>;
-    let y: ScaleLinear<number, number>;
-    let z: ScaleOrdinal<number, string>;
-    let xAxis: Axis<number>;
-    let yAxis: Axis<number>;
-    let brush: BrushBehavior<any>;
-    let idleTimeout: number | null;
-    let idleDelay: number;
-
-    function brushended(this: SVGGElement) {
-      let e = <D3BrushEvent<any>>d3.event;
-      let s: BrushSelection = e.selection;
-      if (!s) {
-        if (!idleTimeout) {
-          self.ngZone.runOutsideAngular(() => {
-            idleTimeout = window.setTimeout(idled, idleDelay);
-          });
-          return idleTimeout;
-        }
-        x.domain(x0);
-        y.domain(y0);
-      } else {
-        x.domain([s[0][0], s[1][0]].map(x.invert, x));
-        y.domain([s[1][1], s[0][1]].map(y.invert, y));
-        d3Svg.select<SVGGElement>('.brush').call(brush.move, null);
-      }
-      zoom();
-    }
-
-    function idled() {
-      idleTimeout = null;
-    }
-
-    function zoom() {
-      let t: Transition<SVGSVGElement, any, null, undefined> = d3Svg.transition().duration(750);
-      d3Svg.select<SVGGElement>('.axis--x').transition(t).call(xAxis);
-      d3Svg.select<SVGGElement>('.axis--y').transition(t).call(yAxis);
-      d3Svg.selectAll<SVGCircleElement, [number, number, number]>('circle').transition(t)
-        .attr('cx', function (d) { return x(d[0]); })
-        .attr('cy', function (d) { return y(d[1]); });
-    }
-
-
-    if (this.parentNativeElement !== null) {
-
-      d3ParentElement = d3.select(this.parentNativeElement);
-      d3Svg = this.d3Svg = d3ParentElement.select<SVGSVGElement>('svg');
-
-      width = +d3Svg.attr('width');
-      height = +d3Svg.attr('height');
-
-      d3G = d3Svg.append<SVGGElement>('g');
-      random = d3.randomNormal(0, 0.2);
-      sqrt3 = Math.sqrt(3);
-      points0 = d3.range(300).map(function (): [number, number, number] { return [random() + sqrt3, random() + 1, 0]; });
-      points1 = d3.range(300).map(function (): [number, number, number] { return [random() - sqrt3, random() + 1, 1]; });
-      points2 = d3.range(300).map(function (): [number, number, number] { return [random(), random() - 1, 2]; });
-      points = d3.merge([points0, points1, points2]);
-
-
-      k = height / width;
-      x0 = [-4.5, 4.5];
-      y0 = [-4.5 * k, 4.5 * k];
-      x = d3.scaleLinear().domain(x0).range([0, width]);
-      y = d3.scaleLinear().domain(y0).range([height, 0]);
-      z = d3.scaleOrdinal<number, string>(d3.schemeCategory10);
-
-      xAxis = d3.axisTop<number>(x).ticks(12);
-      yAxis = d3.axisRight<number>(y).ticks(12 * height / width);
-
-      brush = d3.brush().on('end', brushended);
-      idleDelay = 350;
-
-      d3Svg.selectAll<SVGCircleElement, any>('circle')
-        .data(points)
-        .enter().append<SVGCircleElement>('circle')
-        .attr('cx', function (d) { return x(d[0]); })
-        .attr('cy', function (d) { return y(d[1]); })
-        .attr('r', 2.5)
-        .attr('fill', function (d) { return z(d[2]); });
-
-      d3Svg.append<SVGGElement>('g')
-        .attr('class', 'axis axis--x')
-        .attr('transform', 'translate(0,' + (height - 10) + ')')
-        .call(xAxis);
-
-      d3Svg.append<SVGGElement>('g')
-        .attr('class', 'axis axis--y')
-        .attr('transform', 'translate(10,0)')
-        .call(yAxis);
-
-      d3Svg.selectAll('.domain')
-        .style('display', 'none');
-
-      d3Svg.append<SVGGElement>('g')
-        .attr('class', 'brush')
-        .call(brush);
-
-    }
-
+    this.initSvg()
+    this.initAxis();
+    this.drawAxis();
+    this.drawLine();
   }
+
+  private initSvg() {
+    this.svg = d3.select("svg")
+                 .append("g")
+                 .attr("transform", "translate(" + this.margin.left + "," + this.margin.top + ")");;
+  }
+
+  private initAxis() {
+    this.x = d3Scale.scaleTime().range([0, this.width]);
+    this.y = d3Scale.scaleLinear().range([this.height, 0]);
+    this.x.domain(d3Array.extent(Stocks, (d) => d.date ));
+    this.y.domain(d3Array.extent(Stocks, (d) => d.value ));
+  }
+
+  private drawAxis() {
+
+    this.svg.append("g")
+          .attr("class", "axis axis--x")
+          .attr("transform", "translate(0," + this.height + ")")
+          .call(d3Axis.axisBottom(this.x));
+
+    this.svg.append("g")
+          .attr("class", "axis axis--y")
+          .call(d3Axis.axisLeft(this.y))
+          .append("text")
+          .attr("class", "axis-title")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Price ($)");
+  }
+
+  private drawLine() {
+    this.line = d3Shape.line()
+                       .x( (d: any) => this.x(d.date) )
+                       .y( (d: any) => this.y(d.value) );
+
+    this.svg.append("path")
+            .datum(Stocks)
+            .attr("class", "line")
+            .attr("d", this.line);
+  }
+
 
 }
