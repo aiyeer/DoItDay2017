10a179efcccb402004dbaf9c5d3cc8da4ea307b1
Bharath Janumpally <bjanumpa@tibco.com>
2017-06-16 10:19:35 -0700

    Created model typescripts

---
+++ frontend/src/main/frontend/src/app/d3-demos/brush-zoom-2/AnalysisRouting.ts
@@ -0,0 +1,5 @@
+export interface AnalysisRouting {
+  id: number;
+  analysisId: string;
+serviceId: string;
+}
---
+++ frontend/src/main/frontend/src/app/d3-demos/brush-zoom-2/Communication.ts
@@ -0,0 +1,8 @@
+export interface AnalysisRouting {
+  id: number;
+  fromId: string;
+  toId: string;
+  canCommunicate:boolean;
+  canCommunicate:string;
+  lastModified:Date;
+}
