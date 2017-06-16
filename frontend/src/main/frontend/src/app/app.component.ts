01a9393f24cf3618c63fa5e968015119610acf27
Anjana Iyer <anjiyer@tibco.com>
2017-06-16 06:13:27 -0700

    Autorefresh for service calls.

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,26 +1,34 @@
 import { Component } from '@angular/core';
 import 'rxjs/add/operator/map';
 import {AppService} from './services/app.service';
+import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'D3 Angular Demo';
+ 	title = 'D3 Angular Demo';
 
-  data: any = {};
+ 	data: any = {};
+ 	n: number = 0;
 
-  constructor(private appService: AppService) {
+  	constructor(private appService: AppService) { 
+  		this.autoRefresh();
+  	}
 
-  	this.getAnalysis();
+  	autoRefresh() {
+  		IntervalObservable.create(10000).subscribe(() => {
+    		console.log('hi');
+    		this.getAnalysis();
+    	});
+  	}
 
-  }
-
-  getAnalysis() {
-	  this.appService.getAllAnalysis().subscribe(data => {
-	  	this.data = data
-	  })
-  }
+	getAnalysis() {
+	  	this.appService.getAllAnalysis().subscribe(data => {
+	  		this.data = data
+	  	});
+  	}
 }
